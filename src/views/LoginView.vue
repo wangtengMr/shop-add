75f430c82cd0c306637c8e52b660001f6e15f094
wangyi <1209187811@qq.com>
2023-04-07 16:27:46 +0800

    检测husky功能

---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>page</div>
+</template>
+
+<script>
+import { ref, reactive, toRefs } from 'vue'
+import { useStore } from 'vuex'
+import { useRoute, useRouter } from 'vue-router'
+export default {
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+    const route = useRoute()
+    const data = reactive({
+      list: [1, 3, 3, 3, 3, 3.1, 31, 212, 121, 21],
+      bosy: [2, 3, 1, 5, 56]
+    })
+    return {
+      data
+    }
+  },
+  components: {}
+}
+</script>
+
+<style></style>
